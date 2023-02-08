7829933a641683675926c07e1f08026eb70f0ee8
unknown <inzamam.cu@gmail.com>
2023-02-08 21:41:48 +0800

    meta data added"

--- pages/_app.js
+++ pages/_app.js
@@ -61,6 +61,16 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Inzamamul Haque</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="image" property="og:image" content="/thumbnail.png" />
+        <meta name="author" content="Inzamamul Haque" />
+        <meta
+          name="description"
+          content="This is my portfolio. Here I have shared my skills and projects"
+        />
+      </Head>
       <Preloader load={load} />
 
       {!load && (
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Inzamamul Haque</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="image" property="og:image" content="/thumbnail.png" />
-        <meta name="author" content="Inzamamul Haque" />
-        <meta
-          name="description"
-          content="This is my portfolio. Here I have shared my skills and projects"
-        />
         <link rel="icon" href="/fav.png" />
         <link
           rel="stylesheet"
